0df8f47f0f6c0b2e7f74f985fb28a94cd0c4bfff
srinu2616 <balayya375@gmail.com>
2025-12-24 10:28:49 +0530

    deleted

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
     try {
         mongoose.connection.on('connected', () => console.log("Database Connected"));
-        await mongoose.connect(`${process.env.MONGODB_URI}`);
+        mongoose.connect(`${process.env.MONGODB_URI}`);
     } catch (error) {
         console.log(error.message);
     }
